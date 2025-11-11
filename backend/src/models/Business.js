5cabdf49dc297f9899136a1df4cb39c4e21d1630
ImAnEnvoy <sureteam460@gmail.com>
2025-11-11 21:46:29 +0100

    finishing and exporting business schema

--- backend/src/models/Business.js
+++ backend/src/models/Business.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 
-const businessSchema = new mongoose.Schema({
-  userId: { type: String, index: true },
-  businessName: { type: String, required: true },
-  category: { type: String, required: true },
-  state: { type: String, required: true },
-  city: { type: String, required: true },
-  imagePath: { type: String }
-}, {timestamps});
+const businessSchema = new mongoose.Schema(
+  {
+    userId: { type: String, index: true },
+    businessName: { type: String, required: true, trim: true },
+    category: { type: String, required: true },
+    state: { type: String, required: true, trim: true },
+    city: { type: String, required: true, trim: true },
+    imagePath: { type: String }
+  }, 
+  { timestamps }
+);
 
 export default mongoose.model("Business", businessSchema);
